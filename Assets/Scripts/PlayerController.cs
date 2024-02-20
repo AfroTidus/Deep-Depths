44e096cfe95539ca72ce7c9a5cf37fd715cb2e97
AfroTidus <AfroTidus@gmail.com>
2024-02-20 00:43:55 +0000

    Version 1 Complete
    Now a functional Game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
@@ -13,7 +14,22 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rigidbody;
     private bool launching;
     private float turnDirection;
-    string curLevel;
+    string curScene;
+    string nextScene;
+    bool fire1Collected;
+    bool fire2Collected;
+    bool fire3Collected;
+    public Light2D EndLight;
+    public Light2D Surroundlight;
+    public Light2D Conelight;
+    public GameObject contactOne;
+    public GameObject contactTwo;
+    public GameObject contactThree;
+    public GameObject contactFinal;
+    public AudioSource hitSound;
+    public AudioSource ominousSound;
+    public AudioSource goodSound;
+    public AudioSource badSound;
 
     void Awake()
     {
@@ -22,9 +38,16 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
-        curLevel = SceneManager.GetActiveScene().name;
+        curScene = SceneManager.GetActiveScene().name;
+        if (curScene == "Menu")
+            nextScene = "MainLevel";
+        else if (curScene == "MainLevel")
+            nextScene = "Finish";
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        fire1Collected = false;
+        fire2Collected = false;
+        fire3Collected = false;
     }
 
     void Update()
@@ -60,8 +83,51 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.tag =="Wall")
         {
-            print("hit");
+            //print("hit");
             takeDamage(20);
+            hitSound.Play();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Flame")
+        {
+            Destroy(coll.gameObject);
+            Destroy(contactThree); contactThree = null;
+            fire1Collected = true;
+            ominousSound.Play();
+        }
+        if (coll.gameObject.tag == "BFlame")
+        {
+            Destroy(coll.gameObject);
+            Destroy(contactOne); contactOne = null;
+            fire2Collected = true;
+            Surroundlight.pointLightOuterRadius = 3.0f;
+            ominousSound.Play();
+        }
+        if (coll.gameObject.tag == "GFlame")
+        {
+            Destroy(coll.gameObject);
+            Destroy(contactTwo); contactTwo = null;
+            fire3Collected = true;
+            maxHealth += 40;
+            currentHealth = maxHealth;
+            healthBar.SetMaxHealth(maxHealth);
+            ominousSound.Play();
+        }
+        if (coll.gameObject.tag == "Finish")
+        {
+            EndLight.intensity = 2.0f;
+            ominousSound.Play();
+            if (fire1Collected && fire2Collected && fire3Collected)
+            {
+                StartCoroutine(GoodEnd());
+            }
+            else
+            {
+                StartCoroutine(BadEnd());
+            }
         }
     }
 
@@ -81,6 +147,29 @@ public class PlayerController : MonoBehaviour
     {
         // reload the level in 2 seconds
         yield return new WaitForSeconds(1);
-        SceneManager.LoadScene(curLevel);
+        SceneManager.LoadScene(curScene);
+    }
+    IEnumerator GoodEnd()
+    {
+        yield return new WaitForSeconds(1.5f);
+        goodSound.Play();
+        yield return new WaitForSeconds(2);
+        EndLight.intensity = 0.0f;
+        Conelight.intensity = 0.0f;
+        Surroundlight.intensity = 0.0f;
+        yield return new WaitForSeconds(0.5f);
+        Destroy(contactFinal); contactFinal = null;
+        yield return new WaitForSeconds(2);
+        EndLight.intensity = 2.0f;
+        Conelight.intensity = 1;
+        Surroundlight.intensity = 1;
+        SceneManager.LoadScene(nextScene);
+    }
+    IEnumerator BadEnd()
+    {
+        yield return new WaitForSeconds(1.5f);
+        badSound.Play();
+        yield return new WaitForSeconds(0.75f);
+        SceneManager.LoadScene(curScene);
     }
 }
